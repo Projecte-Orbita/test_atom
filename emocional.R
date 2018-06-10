d7e9444394a37311510aea7aa9cadfdf5465f6f2
Eudald <eudald@correig.net>
2018-06-10 13:26:25 +0200

    gràfics emocional

--- emocional.R
+++ emocional.R
@@ -34,13 +34,6 @@ informe_emocional_petits <- function(index, emocional){
   return(futur);
 }
 
-
-
-
-
-
-
-
 # an\`{a}lisi emocional cicles mitj\`{a} i superior
 
 # assumim que estem rebent una matriu de N_nensx17 amb totes les respostes d'emocional
@@ -57,7 +50,6 @@ informe_emocional_petits <- function(index, emocional){
 #emocional <- read.csv('emocional.csv', header=FALSE, fileEncoding = "UTF-8");
 
 informe_emocional <- function(index, emocional){  
-  
   #
   futur=FALSE;
   lleus=0;
@@ -94,11 +86,13 @@ informe_emocional <- function(index, emocional){
     
     for(j in nb_pregs){  
       
-      if (!is.na(ambits[[(i-1)/4+1]][[3]][[j]][1]) && !is.na(emocional_nou[i+j]) && emocional_nou[i+j] == ambits[[(i-1)/4+1]][[3]][[j]][1]){greu <- c(greu, list(paraula(ambits[[(i-1)/4+1]][[3]][[j]][1]), j));}
+      if (!is.na(ambits[[(i-1)/4+1]][[3]][[j]][1]) && !is.na(emocional_nou[i+j]) 
+          && emocional_nou[i+j] == ambits[[(i-1)/4+1]][[3]][[j]][1])
+        {greu <- c(greu, list(paraula(ambits[[(i-1)/4+1]][[3]][[j]][1]), j));}
       
-      else if (!is.na(ambits[[(i-1)/4+1]][[3]][[j]][2]) && !is.na(emocional_nou[i+j]) && emocional_nou[i+j]== ambits[[(i-1)/4+1]][[3]][[j]][2]){lleu <- c(lleu, list(paraula(ambits[[(i-1)/4+1]][[3]][[j]][2]), j));}}
-    
-    
+      else if (!is.na(ambits[[(i-1)/4+1]][[3]][[j]][2]) && !is.na(emocional_nou[i+j]) 
+               && emocional_nou[i+j]== ambits[[(i-1)/4+1]][[3]][[j]][2])
+        {lleu <- c(lleu, list(paraula(ambits[[(i-1)/4+1]][[3]][[j]][2]), j));}}
     
     if(length(greu) != 0 | length(lleu) != 0)
     {cat("
@@ -108,17 +102,27 @@ informe_emocional <- function(index, emocional){
          ", sep = "");
       afectat <- afectat + 1}
     
-    if(length(greu) != 0){cat("\\item Factors de \\emph{risc greu}: El/la ", as.character(emocional_nou[1]), " ha marcat que ", sep = "");               
-      if(length(greu) != 2){sapply(1:(floor(length(greu)/2)-1), function (x) cat(greu[[2*x-1]], ambits[[(i-1)/4+1]][[2]][as.numeric(greu[[2*x]])], ", ", sep = ""));}
+    if(length(greu) != 0){cat("\\item Factors de \\emph{risc greu}: El/la ", 
+                              as.character(emocional_nou[1]), " ha marcat que ", sep = "");               
+      if(length(greu) != 2){sapply(1:(floor(length(greu)/2)-1), 
+                                   function (x) cat(greu[[2*x-1]], 
+                          ambits[[(i-1)/4+1]][[2]][as.numeric(greu[[2*x]])], ", ", sep = ""));}
       
-      cat(if(length(greu) != 2)"i ", greu[[as.numeric(length(greu)-1)]], ambits[[(i-1)/4+1]][[2]][as.numeric(greu[[length(greu)]])], ". ", sep = ""); futur = TRUE;
+      cat(if(length(greu) != 2)"i ", greu[[as.numeric(length(greu)-1)]], 
+          ambits[[(i-1)/4+1]][[2]][as.numeric(greu[[length(greu)]])], ". ", sep = ""); 
+      futur = TRUE;
     }
     
     
-    if(length(lleu) != 0){cat("\\item Factors de \\emph{risc lleu}: El/la ", as.character(emocional_nou[1]), " ha marcat que ", sep = "");               
-      if(length(lleu) != 2){sapply(1:(floor(length(lleu)/2)-1), function (x) cat(lleu[[2*x-1]], ambits[[(i-1)/4+1]][[2]][as.numeric(lleu[[2*x]])], ", ", sep = ""));}
+    if(length(lleu) != 0){cat("\\item Factors de \\emph{risc lleu}: El/la ", 
+                              as.character(emocional_nou[1]), " ha marcat que ", sep = "");               
+      if(length(lleu) != 2){sapply(1:(floor(length(lleu)/2)-1), 
+                                   function (x) cat(lleu[[2*x-1]], 
+                          ambits[[(i-1)/4+1]][[2]][as.numeric(lleu[[2*x]])], ", ", sep = ""));}
       
-      cat(if(length(lleu) != 2)"i ", lleu[[as.numeric(length(lleu)-1)]], ambits[[(i-1)/4+1]][[2]][as.numeric(lleu[[length(lleu)]])], ". ", sep = ""); lleus <- lleus + 1;
+      cat(if(length(lleu) != 2)"i ", lleu[[as.numeric(length(lleu)-1)]], 
+          ambits[[(i-1)/4+1]][[2]][as.numeric(lleu[[length(lleu)]])], ". ", sep = ""); 
+      lleus <- lleus + 1;
     }
     
     if(length(greu) != 0 | length(lleu) != 0)
@@ -131,7 +135,6 @@ informe_emocional <- function(index, emocional){
   if(lleus>2){futur=TRUE};
   return(futur);
     }
-
 
 paraula <- function(num){
   if(num == 1){return("mai ")};
