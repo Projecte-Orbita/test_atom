43d8eba336b08f82a3b883162d9a59d532bfcd6c
eudald <eudald@correig.net>
2019-04-05 14:31:22 +0200

    canviar noms per números ja funciona

--- canviar_noms_per_numeros.R
+++ canviar_noms_per_numeros.R
@@ -4,6 +4,7 @@
 Sys.setlocale(category="LC_ALL", locale = "Catalan")
 
 noms_per_nums = function(excel_nums, carpetes_noms){
+  require(stringr)
   # Importem els números de matrícula, comptant que tenim un excel amb n columnes ben ordenades
   nums = as.data.frame(read_xlsx(excel_nums, sheet = 1))
   dirs = list.dirs(carpetes_noms)
@@ -11,8 +12,18 @@ noms_per_nums = function(excel_nums, carpetes_noms){
   
   i=1
   for (di in dirs){
-    numeros = as.vector(nums[complete.cases(nums[,i]), i])
+    current_nums = as.vector(t(nums[,i]))
+    numeros = current_nums[complete.cases(current_nums)]
     files = list.files(di)
+    # Hem d'ordenar els fitxers per cognom, igual que les matrícules a l'excel.
+    noms = str_split_fixed(files, "_", 3)
+    noms = paste0(noms[,2], "_", noms[,3])
+    noms_ordenats = sort(noms)
+    index_dels_noms = c()
+    for (nom in noms_ordenats){
+      index_dels_noms = c(index_dels_noms, match(nom, noms))
+    }
+    files = files[index_dels_noms]
     j=1
     for (fi in files){
       aa = file.rename(file.path(di, fi), file.path(di,paste0(numeros[j], '.pdf')))
